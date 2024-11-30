ad3c0dcea3536640508c9193e6415923c40c5f83
CrossDiego2 <00001105774594sp@al.educacao.sp.gov.br>
2024-11-29 21:08:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao (
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao (
+    'Matemática',
+    'Todo número POSITIVO multiplicado por -1 fica?',
+    'Negativo'
+)
+
+criaCartao (
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma taréfa'
+)
+
+criaCartao (
+    'Inglês',
+    'Como se diz feliz em inglês?',
+    'Happy!'
+
+)
